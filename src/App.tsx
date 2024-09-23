07669025b9a54fb610949eebc28da350ddd4e2bb
Apexif <gerencia@apexif.com>
2024-09-23 06:28:39 -0500

    Descripción de los cambios

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './app/page';
+import AuthPage from './app/auth/page';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+const App = () => {
+    return (
+        <SpeedInsights>
+            <HomePage />
+            <AuthPage />
+        </SpeedInsights>
+    );
+};
+
+export default App;
